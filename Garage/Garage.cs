66bf93cace792a32b5a18f4a2f38270d4aba21ff
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-09-30 10:23:44 +0200

    Garage classes

---
+++ Garage/Garage.cs
@@ -0,0 +1,34 @@
+﻿using Garage.Vehicles;
+using System.Collections;
+
+
+namespace Garage.Garage
+{
+    internal class Garage<T> : IEnumerable<T> where T : Vehicle
+    {
+        private T[] vehicles;
+        //counter for the vihacles in the garage
+        private int count = 0;
+
+        public int Capacity { get; private set; }
+
+        public Garage(int capacity)
+        {
+            Capacity = capacity;
+            vehicles = new T[capacity];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return vehicles[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Garage/GarageHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Garage.Garage
+{
+    internal class GarageHandler
+    {
+    }
+}
