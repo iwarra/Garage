340488a7fffbbe64e5b1939583b406bcc59959b0
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-10-01 10:44:20 +0200

    Started menu work on search and print by type

--- MenuHelpers.cs
+++ MenuHelpers.cs
@@ -2,13 +2,12 @@
 {
     internal class MenuHelpers
     {
-
         public const string Print = "1";
         public const string PrintByType = "2";
         public const string Add = "3";
         public const string Remove = "4";
         public const string AddGarage = "5";
-        public const string OpenSearch = "6";
+        public const string Search = "6";
         public const string Quit = "Q";
         public static void ShowMainMenu()
         {
@@ -18,15 +17,8 @@
                    $"\n{Add}: Add a vehicle" +
                    $"\n{Remove}: Remove a vehicle" +
                    $"\n{AddGarage}: Add a new garage and its capacity" +
-                   $"\n{OpenSearch}: Open the search bar" +
+                   $"\n{Search}: Open the search bar" +
                    $"\n{Quit}: Quit");
         }
-
-        //public static void ShowSearchMenu()
-        //{
-        //    Console.WriteLine(
-        //        $"\n{Print}: Show all vehicles"
-        //        );
-        //}
     }
 }
