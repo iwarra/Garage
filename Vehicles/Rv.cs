56881c8626c9401a84821b20a97b6448fb498e01
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-10-01 10:42:23 +0200

    Added to more vehicle subclasses

---
+++ Vehicles/Rv.cs
@@ -0,0 +1,9 @@
+﻿namespace GarageProject.Vehicles
+{
+    internal class Rv : Vehicle
+    {
+        public Rv(string registrationNr, string color, uint nrOfWheels, uint length) : base(registrationNr, color, nrOfWheels)
+        {
+        }
+    }
+}
---
+++ Vehicles/Truck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageProject.Vehicles
+{
+    internal class Truck : Vehicle
+    {
+        public Truck(string registrationNr, string color, uint nrOfWheels, bool isLoaded ) : base(registrationNr, color, nrOfWheels)
+        {
+        }
+    }
+}
